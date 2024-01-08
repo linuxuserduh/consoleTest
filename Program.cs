874156e8fe8c4756c3fe3680d1e5d1e6185e9479
bababooey <115456907+linuxuserduh@users.noreply.github.com>
2024-01-08 21:08:35 +0800

    My BEST (not really) auto-rpg text-line program

---
+++ Program.cs
@@ -0,0 +1,49 @@
+﻿Random rnd = new Random();
+stat Status = new stat();
+enemy monster = new enemy();
+
+int patk;
+int eatk;
+
+Console.WriteLine("Attributes:\nHealth = " + Status.hp + $"\nAttack = {Status.atk[0]}-{Status.atk[1]}" + "\nDefence = " + Status.def);
+Console.ReadKey();
+
+while(true)
+{
+	patk = rnd.Next(Status.atk[1]);
+	eatk = rnd.Next(monster.atk[1]);
+	monster.hp -= patk;
+	Status.hp -= eatk - Status.def;
+
+	Console.WriteLine("The enemy has taken -" + patk + "damage.\nEnemy's HP left: " + monster.hp);
+	Console.ReadKey();
+	if (monster.hp <= 0)
+	{
+		Console.WriteLine("The enemy has fallen. Victory!");
+		Console.ReadKey();
+		break;
+	}
+
+	Console.WriteLine("You have taken -" + eatk + " damage.\nYour HP left: " + Status.hp);
+	Console.ReadKey();
+	if (Status.hp <= 0)
+	{
+		Console.WriteLine("You have been defeated!");
+		Console.ReadKey();
+		break;
+	}
+}
+
+
+class stat
+{
+	public int hp = 100;
+	public int[] atk = {20, 24};
+	public int def = 10;
+}
+
+class enemy
+{
+	public int hp = 100;
+	public int[] atk = {30, 32};
+}
